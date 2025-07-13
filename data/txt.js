b582440413191118ba8a1b4438291eaf89172a51
zyh9949 <zyhxxschina9949@outlook.com>
2025-07-13 22:55:22 +0800

    111

--- data/txt.js
+++ data/txt.js
@@ -1,8 +1,7 @@
 // 等页面加载完成后再执行
 window.addEventListener('DOMContentLoaded', () => {
     // 从 data/note.txt 获取文本
-    fetch('http://zyh9949.top:5245/d/zz%E7%A7%81%E6%9C%89%E7%9B%AE%E5%BD%95/%E7%AC%AC%E4%B8%89%E6%96%B9%E4%BA%91%E7%9B%98%E6%9C%8D%E5%8A%A1/ZCloud/webnote.txt?sign=Qh8KLQqD10DOz9zvKyi66oksF_GI0qxDTUh-lrdfVr8=:0')
-        .then(response => {
+    fetch('.data/note.txt')
             if (!response.ok) {
                 throw new Error('无法加载 note.txt：' + response.status);
             }
